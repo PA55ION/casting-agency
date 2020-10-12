4ae62644473d4aa3233d3b8429313f3bab8bcd83
Mai Campbell <jirallc@gmail.com>
2020-10-12 18:01:24 -0400

    feat: add actor form to the frontend

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from flask_cors import CORS
 from models import setup_db, Movies, Actors, rollback
 from auth import AuthError, requires_auth
 from flask_wtf import FlaskForm
+from flask_wtf import Form
 from forms import *
 import sys
 
@@ -47,20 +48,23 @@ def create_app(test_config=None):
 
     @app.route('/movies')
     def get_movies():
-        try:
-            movies = Movies.query.all()
-            all_movies = [movie.format() for movie in movies]
-            print(all_movies)
-            return jsonify({
-                'success': True,
-                'movies': all_movies,
-            })
-        except Exception as e:
-            print(e)
+        movies = Movies.query.all();
+        return render_template('pages/movies.html', movies=movies)
+        # try:
+        #     movies = Movies.query.all()
+        #     all_movies = [movie.format() for movie in movies]
+        #     print(all_movies)
+        #     return jsonify({
+        #         'success': True,
+        #         'movies': all_movies,
+        #     })
+        # except Exception as e:
+        #     print(e)
 
     @app.route('/movies/<int:movie_id>', methods=['DELETE'])
-    @requires_auth('delete:movies')
-    def delete_movie(token, movie_id):
+    def delete_movie(movie_id):
+    # @requires_auth('delete:movies')
+    # def delete_movie(token, movie_id):
         try:
             movie = Movies.query.get(movie_id)
             name = movie.title
@@ -77,8 +81,9 @@ def create_app(test_config=None):
             }), 404
 
     @app.route('/movies', methods=['POST'])
-    @requires_auth('post:movies')
-    def add_movie(token):
+    # @requires_auth('post:movies')
+    # def add_movie(token):
+    def add_movie():
         data = request.get_json()
 
         new_title = data.get('title', None)
@@ -111,8 +116,9 @@ def create_app(test_config=None):
             }), 422
 
     @app.route('/movies/<int:movie_id>', methods=['PATCH'])
-    @requires_auth('patch:movies')
-    def update_movie(token, movie_id):
+    # @requires_auth('patch:movies')
+    # def update_movie(token, movie_id):
+    def update_movie(movie_id):
         movie = Movies.query.filter(Movies.id == movie_id).one_or_none()
         if movie:
             try:
@@ -143,63 +149,100 @@ def create_app(test_config=None):
                     'error': 'Bad request'
                 }), 400
 
+    # Get form
+    @app.route('/add_movie', methods=['GET'])
+    def create_movie_form():
+        form = MovieForm()
+        return render_template('forms/new_movies.html')
+
+    @app.route('/movies/<int:movie_id>')
+    def edit_movie(movie_id):
+        form = MovieForm()
+        movie = Movies.query.get(movie_id)
+        form.title.data = movie.title
+        form.release_date.data = movie.release_date
+        form.description.data = movie.description
+        form.image_link.data = movie.image_link
+        return render_template('forms/edit_movie.html', form=form, movie=movie)
+
 # COMMENT endpoints for actors
     @app.route('/actors')
     def get_actors():
-        # actors = Actors.query.all()
-        try:
-            actors = Actors.query.all()
-            actor_list = [actor.format() for actor in actors]
-            print(actor_list)
-            return jsonify({
-                'success': True,
-                'actors': actor_list
-            }), 200
+        actors = Actors.query.all()
+        # try:
+        #     actors = Actors.query.all()
+        #     actor_list = [actor.format() for actor in actors]
+        #     print(actor_list)
+        #     return jsonify({
+        #         'success': True,
+        #         'actors': actor_list
+        #     }), 200
 
-        except Exception:
-            return json.dumps({
-                'success': False,
-                'error': 'Actor not found.'
-            }), 404
+        # except Exception:
+        #     return json.dumps({
+        #         'success': False,
+        #         'error': 'Actor not found.'
+        #     }), 404
 
-        # return render_template('pages/actors.html', actors=actors)
+        return render_template('pages/actors.html', actors=actors)
     # add actor
-    @app.route('/actors', methods=['POST'])
-    @requires_auth('post:actors')
-    def add_actor(token):
+    @app.route('/add_actor', methods=['POST'])
+    # @requires_auth('post:actors')
+    def add_actor():
+        error = False
+        new_name = request.form['name']
+        new_age = request.form['age']
+        new_gender = request.form['gender']
+        new_image_link = request.form['image_link']
+
         try:
-            data = request.get_json()
-            new_name = data.get('name', None)
-            new_age = data.get('age', None)
-            new_gender = data.get('gender', None)
-            new_image_link = data.get('image_link', None)
-            new_description = data.get('description', None)
-
-            actor = Actors(
-                name=new_name,
-                age=new_age,
-                gender=new_gender,
-                image_link=new_image_link,
-                description=new_description)
+            actor = Actors(name=new_name, age=new_age,
+                           gender=new_gender, image_link=new_image_link)
+                           
             actor.insert()
-
-            selection = Actors.query.order_by('id').all()
-            return jsonify({
-                'success': True,
-                'message': 'Successfully added',
-                'actors': len(selection)
-            })
+            flash('Artist ' + request.form['name'] + ' successfully created 🚀')
 
         except Exception:
-            return json.dumps({
-                'success': False,
-                'error': 'Not able to add model at this time',
-            }), 422
+            rollback()
+            error = True
+            abort(400)
+            flash('An error occurred. ' + actor.name + ' could not be listed.')
+
+        return redirect(url_for('get_actors'))
+        # try:
+        #     data = request.get_json()
+        #     new_name = data.get('name', None)
+        #     new_age = data.get('age', None)
+        #     new_gender = data.get('gender', None)
+        #     new_image_link = data.get('image_link', None)
+        #     new_description = data.get('description', None)
+
+        #     actor = Actors(
+        #         name=new_name,
+        #         age=new_age,
+        #         gender=new_gender,
+        #         image_link=new_image_link,
+        #         description=new_description)
+        #     actor.insert()
+
+        #     selection = Actors.query.order_by('id').all()
+        #     return jsonify({
+        #         'success': True,
+        #         'message': 'Successfully added',
+        #         'actors': len(selection)
+        #     })
+
+        # except Exception:
+        #     return json.dumps({
+        #         'success': False,
+        #         'error': 'Not able to add model at this time',
+        #     }), 422
 
 # delete actor endpoint
     @app.route('/actors/<int:actor_id>', methods=['DELETE'])
-    @requires_auth('delete:actors')
-    def delete_actor(token, actor_id):
+    def delete_actor(actor_id):
+    # @requires_auth('delete:actors')
+    # def delete_actor(token, actor_id):
         try:
             actor = Actors.query.get(actor_id)
             name = actor.name
@@ -219,42 +262,73 @@ def create_app(test_config=None):
             }), 404
 
 # edit actor
-    @app.route('/actors/<int:actor_id>', methods=['PATCH'])
-    @requires_auth('patch:actors')
-    def update_actor(token, actor_id):
+    @app.route('/actors/<int:actor_id>', methods=['PATCH', 'POST'])
+    # @requires_auth('patch:actors')
+    # def update_actor(token, actor_id):
+    def update_actor(actor_id):
+        actor = Actors.query.get(actor_id)
 
-        actor = Actors.query.filter(Actors.id == actor_id).one_or_none()
-        if actor:
-            try:
-                data = request.get_json()
-                update_name = data.get('name', None)
-                update_age = data.get('age', None)
-                update_gender = data.get('gender', None)
-                update_image = data.get('image_link', None)
-                update_description = data.get('description', None)
-                if update_name:
-                    actor.name = update_name
-                if update_age:
-                    actor.age = update_age
-                if update_gender:
-                    actor.gender = update_gender
-                if update_image:
-                    actor.image_link = update_image
-                if update_description:
-                    actor.description = update_description
+        try:
+            actor.name = request.form['name']
+            actor.age = request.form['age']
+            actor.gender = request.form['gender']
+            actor.image_link = request.form['image_link']
+            actor.update()
+            flash(request.form['name'] + ' successfully updated 🚀 ')
+        except BaseException:
+            rollback()
+            flash(request.form['name'] + ' cannot be updated! 😞')
 
-                actor.update()
+        return redirect(url_for('get_actors', actor_id=actor_id))
 
-                return jsonify({
-                    'success': True,
-                    'message': 'Successfully updated.',
-                    'actors': actor.format()
-                }), 200
-            except Exception:
-                return json.dumps({
-                    'success': False,
-                    'error': 'Bad request.',
-                }), 400
+
+        # actor = Actors.query.filter(Actors.id == actor_id).one_or_none()
+        # if actor:
+        #     try:
+        #         data = request.get_json()
+        #         update_name = data.get('name', None)
+        #         update_age = data.get('age', None)
+        #         update_gender = data.get('gender', None)
+        #         update_image = data.get('image_link', None)
+        #         update_description = data.get('description', None)
+        #         if update_name:
+        #             actor.name = update_name
+        #         if update_age:
+        #             actor.age = update_age
+        #         if update_gender:
+        #             actor.gender = update_gender
+        #         if update_image:
+        #             actor.image_link = update_image
+        #         if update_description:
+        #             actor.description = update_description
+
+        #         actor.update()
+
+        #         return jsonify({
+        #             'success': True,
+        #             'message': 'Successfully updated.',
+        #             'actors': actor.format()
+        #         }), 200
+        #     except Exception:
+        #         return json.dumps({
+        #             'success': False,
+        #             'error': 'Bad request.',
+        #         }), 400
+
+    @app.route('/add_actor', methods=['GET'])
+    def create_actor_form():
+        form = ActorForm()
+        return render_template('forms/new_actors.html', form=form)
+
+    @app.route('/actors/<int:actor_id>')
+    def edit_actor(actor_id):
+        form = ActorForm()
+        actor = Actors.query.get(actor_id)
+        form.name.data = actor.name
+        form.age.data = actor.age
+        form.gender.data = actor.gender
+        form.image_link.data = actor.image_link
+        return render_template('forms/edit_actor.html', form=form, actor=actor)
 
 # Error handler
 
--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField
+from flask_wtf import Form
+from wtforms import StringField, DateField, SelectMultipleField
 from wtforms.validators import DataRequired, AnyOf, URL
 
 
-class MovieForm(FlaskForm):
+class MovieForm(Form):
     title = StringField(
         'title', validators=[DataRequired(message='Please enter a title.')]
     )
@@ -17,9 +18,35 @@ class MovieForm(FlaskForm):
     release_date = StringField(
         'release_date', validators=[DataRequired()]
     )
+    genres = SelectMultipleField(
+        # TODO implement enum restriction
+        'genres', validators=[DataRequired()],
+        choices=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
+    )
+    
 
 
-class ActorForm(FlaskForm):
+class ActorForm(Form):
     name = StringField(
         'name', validators=[DataRequired()]
     )
@@ -31,4 +58,7 @@ class ActorForm(FlaskForm):
     )
     image_link = StringField(
         'image_link', validators=[DataRequired()]
+    )
+    description = StringField(
+        'descripton'
     )
